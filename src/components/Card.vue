8ed6f9e5befb5fb6b1e6c4f27db4c38d9fc03785
mghextreme <matiashenschel@hotmail.com>
2021-02-06 12:56:19 -0300

    Added Save the Date title, animation when idle.

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,12 +3,17 @@
     <div class="names">
       <img src="../assets/images/matias-e-aniele.svg" />
     </div>
+    <div class="title">
+      <span class="en">Save the date</span>
+      <span class="split">-</span>
+      <span class="pt">Guarde esta data</span>
+    </div>
     <div class="date">
       <span class="pre-date"></span>
-      <CardHole char="3"></CardHole>
+      <CardHole char="3" cursorDelay="12000"></CardHole>
       <CardHole char="1"></CardHole>
       <span class="mid-date"></span>
-      <CardHole char="O"></CardHole>
+      <CardHole char="O" cursorDelay="17000"></CardHole>
       <CardHole char="U"></CardHole>
       <CardHole char="T"></CardHole>
     </div>
@@ -37,25 +42,54 @@ export default class Card extends Vue {}
   left: 15px;
   right: 15px;
   max-width: 480px;
-  height: 260px;
-  transform: translateY(-114px);
+  height: 290px;
+  transform: translateY(-155px);
   background: center repeat url('../assets/images/paper-texture.jpg');
   background-color: white;
   box-shadow: 2px 2px 3px 0 rgba(0, 0, 0, 0.7);
 
   .names {
     position: absolute;
-    top: 20px;
+    top: 25px;
     left: 10px;
     right: 10px;
     max-width: 426px;
     height: 52px;
     opacity: .8;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  .title {
+    position: absolute;
+    top: 105px;
+    left: 10px;
+    right: 10px;
+    letter-spacing: 0.12em;
+    font-size: 1.1em;
+    font-family: $font-josefin;
+    text-align: center;
+    text-transform: uppercase;
+    opacity: 0.5;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+
+    span {
+      display: inline-block;
+
+      &.split {
+        margin: 0 7px;
+      }
+    }
   }
 
   .date {
     position: absolute;
-    top: 105px;
+    top: 146px;
     left: 10px;
     right: 10px;
     display: flex;
@@ -85,15 +119,15 @@ export default class Card extends Vue {}
 
 @media screen and (max-width: 340px) {
   .card {
-    height: 240px;
-    transform: translateY(-104px);
+    height: 270px;
+    transform: translateY(-119px);
 
     .names {
       top: 15px;
     }
 
     .date {
-      top: 72px;
+      top: 87px;
       max-width: 138px;
       flex-wrap: wrap;
 
--- src/components/CardHole.vue
+++ src/components/CardHole.vue
@@ -1,7 +1,10 @@
 <template>
   <div v-bind:class="{ 'card-hole': true, 'on': on }">
-    <div class="circle" v-on:click="on = true">
+    <div class="circle" v-on:click="activate">
       {{ content }}
+    </div>
+    <div class="cursor" v-if="cursorActive">
+      <img src="../assets/images/tap.svg" width="30" />
     </div>
   </div>
 </template>
@@ -12,10 +15,27 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 @Component
 export default class CardHole extends Vue {
   @Prop() readonly char!: string
+  @Prop() readonly cursorDelay!: number
   on = false;
+  cursorActive = false;
 
   get content (): string {
     return this.on ? this.char : ''
+  }
+
+  activate () {
+    this.on = true
+    this.cursorActive = false
+  }
+
+  mounted () {
+    if (this.cursorDelay) {
+      setInterval(() => {
+        if (!this.on) {
+          this.cursorActive = true
+        }
+      }, this.cursorDelay)
+    }
   }
 }
 </script>
@@ -24,6 +44,7 @@ export default class CardHole extends Vue {
 @import '../assets/style/variables';
 
 .card-hole {
+  position: relative;
   margin: 5px;
   -webkit-user-select: none;
   -moz-user-select: none;
@@ -49,6 +70,29 @@ export default class CardHole extends Vue {
       background: $color-background;
       box-shadow: inset 2px 2px 3px 0 rgba(0, 0, 0, 0.7);
     }
+  }
+
+  .cursor {
+    position: absolute;
+    top: 18px;
+    left: 18px;
+
+    animation-name: cursorloop;
+    animation-duration: 700ms;
+    animation-timing-function: ease-out;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+  }
+}
+
+@keyframes cursorloop {
+  from {
+    top: 18px;
+    left: 18px;
+  }
+  to {
+    top: 28px;
+    left: 28px;
   }
 }
 </style>
