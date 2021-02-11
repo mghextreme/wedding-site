2464f7296c6b44e438b14cd03ac4ffcbd9fa2de2
Matias G Henschel <matias.henschel@philips.com>
2021-02-11 08:20:15 -0300

    Add Home page.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,21 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import SaveTheDate from '../views/SaveTheDate.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
+    path: '/savethedate',
+    alias: '/',
     name: 'SaveTheDate',
     component: SaveTheDate
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
   }
 ]
 
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <div class="menu"></div>
+    <div class="highlighted-picture"></div>
+    <div class="verse"></div>
+    <div class="when"></div>
+    <div class="where"></div>
+    <div class="contact-us"></div>
+    <div class="footer"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  metaInfo () {
+    return {
+      title: 'Matias e Aniele'
+    }
+  }
+})
+export default class Home extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.home {
+  width: 100%;
+  height: 100%;
+}
+</style>
